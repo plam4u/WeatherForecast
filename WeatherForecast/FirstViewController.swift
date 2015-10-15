6ea7108caa21485fa3eaa659b92c30476982bfa2
Plamen Andreev <plam4u@gmail.com>
2015-10-15 10:18:07 +0100

    Initial Commit

---
+++ WeatherForecast/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  WeatherForecast
+//
+//  Created by Plamen Andreev on 10/15/15.
+//  Copyright © 2015 Plamen Andreev. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
---
+++ WeatherForecast/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  WeatherForecast
+//
+//  Created by Plamen Andreev on 10/15/15.
+//  Copyright © 2015 Plamen Andreev. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
