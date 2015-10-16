3b6824d13bef746662e0191d39d1cff29b6590d4
Plamen Andreev <plam4u@gmail.com>
2015-10-16 10:09:50 +0100

    prototyped Forecast's UI in Interface Builder

--- WeatherForecast/Classes/Controller/ForecastViewController.swift
+++ WeatherForecast/Classes/Controller/ForecastViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ForecastViewController: UIViewController {
 
+	@IBOutlet weak var tableView: UITableView!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+		tableView.delegate = self;
+		tableView.dataSource = self;
 	}
 
 	override func didReceiveMemoryWarning() {
@@ -23,3 +27,26 @@ class ForecastViewController: UIViewController {
 
 }
 
+extension ForecastViewController : UITableViewDataSource
+{
+	func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+	{
+		return 7;
+	}
+	
+	func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+	{
+		let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+		return cell;
+	}
+	
+	func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat
+	{
+		return tableView.bounds.size.height / round(tableView.bounds.size.height / 90);
+	}
+}
+
+extension ForecastViewController : UITableViewDelegate
+{
+	
+}
